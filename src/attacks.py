f4b4a5e3c1354e788b69aeec46e32bcb72770012
Luã Streit <lua.streit@tuta.io>
2020-10-16 19:03:56 +0200

    Now find adversarial works for many feature changes at a time

--- src/attacks.py
+++ src/attacks.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+from itertools import product
+
 from tqdm import tqdm
 
 from .preprocessing import one_hot_encode
@@ -8,8 +10,7 @@ from .preprocessing import one_hot_encode
 def find_adversarial_examples(srr_model, X, y, can_change):
     """
     Given an SRR model, data points, and the features which can be changed, produces a list of 
-    adversarial examples that have changed the model prediction label, and that are only one
-    feature value away from a real data point.
+    adversarial examples that have changed the model prediction label.
     
     Arguments:
     - srr_model : Trained SRR model
@@ -38,26 +39,23 @@ def find_adversarial_examples(srr_model, X, y, can_change):
     # Create a mapping from features to possible categories
     feat_to_cats = {feat: list(X[feat].unique()) for feat in X.columns}
     
+    # Create a list with tuples of possible feature changes
+    possible_changes = list(product(*[feat_to_cats[feat] for feat in modifiable_features]))
     
-    ## Construct a list of potential adversarial examples by deforming
-    ## exactly one feature at a time from each correctly classified point
+    
+    ## Construct a list of potential adversarial examples by deforming each correctly classified sample
     potential_adversaries = pd.DataFrame()
     
     # Iterate over correctly classified points
     for index, data in tqdm(correctly_classified.iterrows(), total=correctly_classified.shape[0]):
         
-        # Go through features that can be changed
-        for feat in modifiable_features:
+        # Go through precomputed tuples of feature changes
+        for change in possible_changes:
             
-            # Change feature for each of the possible values except original
-            for category in feat_to_cats[feat]:
-                if category == data[feat]:
-                    continue
-                
-                # Modify the original data point and add it to the list
-                deformed = data.copy()
-                deformed[feat] = category
-                potential_adversaries = potential_adversaries.append(deformed)
+            # Modify the original data point and add it to the list
+            deformed = data.copy()
+            deformed.loc[modifiable_features] = change
+            potential_adversaries = potential_adversaries.append(deformed)
     
     # Get the model prediction for the adversaries
     potential_adversaries['label~adversarial'] = srr_model.predict(
@@ -81,4 +79,4 @@ def find_adversarial_examples(srr_model, X, y, can_change):
     
     # Sort the column names to get a more compact view
     return adversaries_and_originals.reindex(sorted(adversaries_and_originals.columns), axis=1)
-
+    
